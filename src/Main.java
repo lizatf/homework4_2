6db630bb1deadd916b0f30ae5f627c55fd4c3a32
lizatf <ltuflina@gmail.com>
2024-05-12 16:35:54 +0200

    print new array with sie from console

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите размер массива: ");
+        int size = scanner.nextInt();
+        int[] array = new int[size];
+        System.out.println(Arrays.toString(array));
+    }
+}
